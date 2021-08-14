f2fd1787a644785f4c1a85e8ca947d35f8c762d9
Joe Hertig <joehertig94@gmail.com>
2021-08-13 23:12:55 -0600

    setting up length function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min) + min);
+
+  return value;
+};
+
+//                                Generate functions
+// randomLower function
+function randomLower() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,23 +22,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
+ // generatePassword function
+// function generatePassword() {
+  
+// }
 
-function generatePassword() {
+// getLength function
+function getLength() {
   // Password Length
-  var promptLength = window.prompt(
-    'Please choose the length of the password. "An Amount with at least 8 characters and no more than 128."'
-  );
+  var promptLength = "";
   
   // Validate prompt answer
-  if (promptLength === "" || promptFight === null) {
-    window.alert("You need to provide a valid answer! Please try again.");
-    //Return function to generatePassword for valid answer
-    return generatePassword();
+  while (promptLength === "" || promptLength === null) {
+    promptLength = prompt('Please choose the length of the password. "An Amount with at least 8 characters and no more than 128."');
   }
+  window.confirm("Is " + promptLength + " the correct length?");
+  return promptLength;
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-generatePassword();
+getLength();
