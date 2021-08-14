560834f18cd1f8c464daa670a5ea5b44a86edda4
Joe Hertig <joehertig94@gmail.com>
2021-08-14 17:02:17 -0600

    completed asking for length of character

--- Develop/script.js
+++ Develop/script.js
@@ -10,36 +10,45 @@ var randomNumber = function (min, max) {
 
 //                                Generate functions
 // randomLower function
-function randomLower() {
-
-}
+function randomLower() {}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- // should be what user input + a value = password
+  // should be what user input + a value = password
   passwordText.value = password;
-
 }
- // generatePassword function
+// generatePassword function
 // function generatePassword() {
-  
+
 // }
 
 // getLength function
 function getLength() {
-  // Password Length
-  var promptLength = "";
-  
-  // Validate prompt answer
-  while (promptLength === "" || promptLength === null) {
-    promptLength = prompt('Please choose the length of the password. "An Amount with at least 8 characters and no more than 128."');
-  }
-  window.confirm("Is " + promptLength + " the correct length?");
-  return promptLength;
-}
+  // asking user for password Length
+  var promptLength = prompt(
+    'Please choose the length of the password. "An Amount with at least 8 characters and no more than 128."'
+  );
 
+  // Validate prompt answer
+  if (promptLength === "" || promptLength === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    // start function over for valid answer
+    return getLength();
+  }
+
+  // validate if input is correct
+  var promptLengthConfirm = window.confirm(
+    "Is " + promptLength + " the correct length?"
+  );
+
+  if (promptLengthConfirm) {
+    randomNumber();
+  } else {
+    getLength();
+  }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
